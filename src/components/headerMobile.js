5c82532d31d675ddf889fabf0a6a7184595519c1
leo-mattar <126821704+leo-mattar@users.noreply.github.com>
2026-06-08 05:16:36 +0100

    mh2

--- src/components/headerMobile.js
+++ src/components/headerMobile.js
@@ -1,15 +1,16 @@
 export default function headerMobile() {
   const navBtn = document.querySelector("[data-header-mobile-btn]");
-  const header = document.querySelector(".c-header");
-  if (navBtn && header) {
-    navBtn.addEventListener("click", () => {
-      header.classList.toggle("is-open");
+  const header = document.querySelector(".navbar");
 
-      if (header.classList.contains("is-open")) {
-        window.lenis.stop();
-      } else {
-        window.lenis.start();
-      }
-    });
-  }
+  if (!navBtn || !header) return;
+
+  navBtn.addEventListener("click", () => {
+    header.classList.toggle("is-open");
+
+    if (header.classList.contains("is-open")) {
+      window.lenis.stop();
+    } else {
+      window.lenis.start();
+    }
+  });
 }
